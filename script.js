a0e5515f0cb0d600eee70c4763e878616b55dbd4
unknown <tussharr24@gmail.com>
2023-08-28 22:58:00 +0530

    js update

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
   });
+
+function firstPageAnim() {
+    var tl = gsap.timline();
+
+    tl.from("#nav", {
+        y: "-10",
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut,
+    })
+    .to("boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duraton: 2,
+        delay: -1,
+        stagger: 0.2,
+    })
+    .from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        duration:1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+    });
+}
